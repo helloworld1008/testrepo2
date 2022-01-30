494998179a51e1dcbf92d9056bfe307c5e54ff54
test <test@test.com>
2022-01-30 21:28:46 +0530

    Added MyDiff code

--- src/MySum.py
+++ src/MySum.py
@@ -2,4 +2,4 @@
 
 def mysum(a,b):
 
-	return a + b 
+	return a + b + 1 
--- test_code.py
+++ test_code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import unittest
-from src import MySum, MyProduct
+from src import MySum, MyProduct, MyDiff
 
 class TestCode(unittest.TestCase):
 
@@ -12,6 +12,10 @@ class TestCode(unittest.TestCase):
 	def test_myproduct(self):
 
 		self.assertEqual(MyProduct.myproduct(9,9),81,"Must be 81")
+
+	def test_mydiff(self):
+
+		self.assertEqual(MyDiff.mydiff(18,9),9,"Must be 9")
 
 if __name__ == "__main__":
 
